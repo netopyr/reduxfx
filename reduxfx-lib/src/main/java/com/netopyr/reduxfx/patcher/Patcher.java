2df9b39cc5c826a737f1abe486c6a9100c08c406
Michael Heinrichs <michael.heinrichs@netopyr.com>
2016-11-20 21:53:18 +0100

    Improved logging

--- reduxfx-lib/src/main/java/com/netopyr/reduxfx/patcher/Patcher.java
+++ reduxfx-lib/src/main/java/com/netopyr/reduxfx/patcher/Patcher.java
@@ -33,7 +33,7 @@ public class Patcher<ACTION> {
 
     public void patch(Parent root, Option<VNode<ACTION>> vRoot, Seq<Patch> patches) {
 
-        LOG.trace("Patches: {}", patches);
+        LOG.trace("Patches:\n{}", patches);
 
         for (final Patch patch : patches) {
             final Node node = vRoot.flatMap(vNode -> findNode(patch.getIndex(), root.getChildrenUnmodifiable().get(0), vNode, 0)).getOrElse(root);
