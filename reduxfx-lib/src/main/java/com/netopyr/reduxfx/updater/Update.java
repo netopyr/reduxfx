e2d573350075645bc602cb687cc40770a31aa904
Michael Heinrichs <michael.heinrichs@netopyr.com>
2017-01-17 08:08:49 +0100

    Improve Update interface

--- reduxfx-lib/src/main/java/com/netopyr/reduxfx/updater/Update.java
+++ reduxfx-lib/src/main/java/com/netopyr/reduxfx/updater/Update.java
@@ -4,29 +4,37 @@ import javaslang.collection.Array;
 import javaslang.collection.Seq;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
+import java.util.Arrays;
+
 public final class Update<STATE> {
 
-    public static <T> Update<T> of(T state, Command... commands) {
-        return Update.of(state, Array.of(commands));
+    public static <T> Update<T> of(T state) {
+        return new Update<>(state);
     }
 
-    public static <T> Update<T> of(T state, Seq<? extends Command> commands) {
+    public static <T> Update<T> of(T state, Command... commands) {
+        return new Update<>(state, Array.of(commands));
+    }
+
+    @SafeVarargs
+    public static <T> Update<T> of(T state, Seq<Command>... commands) {
         return new Update<>(state, commands);
     }
 
     private final STATE state;
-    private final Seq<? extends Command> commands;
+    private final Seq<Command> commands;
 
-    private Update(STATE state, Seq<? extends Command> commands) {
+    @SafeVarargs
+    private Update(STATE state, Seq<Command>... commands) {
         this.state = state;
-        this.commands = commands;
+        this.commands = Arrays.stream(commands).reduce(Array.empty(), Seq::appendAll);
     }
 
     public STATE getState() {
         return state;
     }
 
-    public Seq<? extends Command> getCommands() {
+    public Seq<Command> getCommands() {
         return commands;
     }
 
